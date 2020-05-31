e03ae8b1b8ea6d199c641cf646c760bb7711b125
Cyrus Roshan <cyrusroshan123@gmail.com>
2020-05-30 22:38:01 -0500

    Upgrade to latest parcel, get output working.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import core from '@actions/core'
+import { getInput, setOutput, setFailed } from '@actions/core'
 
 try {
-  const urlPrefix = core.getInput('url-prefix');
-  const apiToken = core.getInput('api-token');
+  const urlPrefix = getInput('url-prefix');
+  const apiToken = getInput('api-token');
 
-  core.setOutput("purged_file_count", 0);
+  setOutput("purged_file_count", 0);
 } catch (error) {
-  core.setFailed(error.message);
+  setFailed(error.message);
 }
