38910bb555855f445e7e077360f920650d1472c6
Cyrus Roshan <cyrusroshan123@gmail.com>
2020-05-30 23:59:34 -0500

    Await async funciton.

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,29 @@ import { getInput, setOutput, setFailed } from '@actions/core';
 import { exec } from 'child_process';
 
 try {
-  const urlPrefix = getInput('url-prefix');
-  const apiToken = getInput('api-token');
-  const gitPath = process.env.GITHUB_WORKSPACE || '.';
+  (async () => {
+    const urlPrefix = getInput('url-prefix');
+    const apiToken = getInput('api-token');
+    const gitPath = process.env.GITHUB_WORKSPACE || '.';
 
-  exec(`git diff --name-only HEAD~1`,
-    {
-      cwd: gitPath,
-    },
-    (error, stdout, stderr) => {
-      if (error) {
-        throw('exec error:' + error.message)
-      }
-      if (stderr) {
-        throw('error running git diff: ' + stderr);
-      }
+    const diffFiles: String[] = await new Promise((resolve, reject) => {
+      exec(`git diff --name-only HEAD~1`, { cwd: gitPath },
+        (error, stdout, stderr) => {
+          if (error) {
+            reject('exec error:' + error.message)
+          }
+          if (stderr) {
+            reject('error running git diff: ' + stderr);
+          }
 
-      const diffFiles = stdout.split('\n').filter((x) => x.length)
-      setOutput("purged_file_count", diffFiles.length);
-    }
-  );
+          const diffFiles = stdout.split('\n').filter((x) => x.length)
+          resolve(diffFiles);
+        }
+      );
+    })
+
+    setOutput("purged_file_count", diffFiles.length);
+  })()
 } catch (error) {
   setFailed(error.message);
 }
